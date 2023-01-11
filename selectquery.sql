767c088f01cf9e23cd694b32244f9b65e69e6adf
Combatd <combatdstar@gmail.com>
2023-01-11 11:57:31 -0800

    Dynamic filter by 2016 and 2017 years

--- selectquery.sql
+++ selectquery.sql
@@ -12,5 +12,15 @@ where "Unit Cost" > 100
 limit 10;
 */
 
+/*
 where Extract (Year from to_date("Ship Date", 'MM/DD/YYYY')) = '2015'
 limit 10;
+*/
+where (Extract (Year from to_date("Ship Date", 'MM/DD/YYYY')) = 
+select max((Extract (Year from to_date("Ship Date", 'MM/DD/YYYY')) from sales)
+or
+Extract (Year from to_date("Ship Date", 'MM/DD/YYYY')) = 
+select max((Extract (Year from to_date("Ship Date", 'MM/DD/YYYY')) - 1 from sales)
+)
+and
+region 'Europe';
